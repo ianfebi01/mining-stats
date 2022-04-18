35abc2dc34a7a6f7e98a5be6ca73997285866b4d
Ian Febi S <ianfebi01@gmail.com>
2022-04-18 21:54:19 +0700

    nyicil front end 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './main.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowUp,
+  faPowerOff,
+  faArrowLeft,
+  faArrowRight,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
@@ -109,7 +116,7 @@ function App() {
                                   </div>
                                   <div className="column is-3 py-1">
                                     <div className="box-percentage is-size-7">
-                                      <FontAwesomeIcon icon={faArrowUp} />
+                                      {/* <FontAwesomeIcon icon={faArrowUp} /> */}
                                       7.5%
                                     </div>
                                   </div>
@@ -143,21 +150,88 @@ function App() {
               <div className="column is-5">
                 <section className="hero next-prev round-corner has-background-white">
                   <div className="hero-body py-4 px-5">
-                    <h1 className="title has-text-primary">December</h1>
+                    <div className="columns">
+                      <div className="column is-8 py-3">
+                        <h1 className="is-size-6 has-text-primary">
+                          March Income
+                        </h1>
+                      </div>
+                      <div className="column is-4 py-3">
+                        <div className="box-percentage is-size-7">
+                          <FontAwesomeIcon icon={faArrowUp} />
+                          7.5%
+                        </div>
+                      </div>
+                    </div>
+                    <div className="columns">
+                      <div className="column pt-0">
+                        <h1 className="title is-size-2 has-text-primary">
+                          0.034768
+                        </h1>
+                      </div>
+                    </div>
+                    <div className="columns">
+                      <div className="column py-1">
+                        <h1 className="title is-size-5 has-text-primary">
+                          ETH
+                        </h1>
+                      </div>
+                    </div>
                   </div>
                 </section>
               </div>
               <div className="column is-2">
-                <section className="hero next-prev round-corner has-background-white">
+                <section className="hero next-prev">
                   <div className="hero-body py-4 px-5">
-                    <h1 className="title has-text-primary">g</h1>
+                    <ul className="arrow has-text-primary">
+                      <li>
+                        <FontAwesomeIcon
+                          className="is-size-2"
+                          icon={faArrowLeft}
+                        />
+                      </li>
+                      <li>Next</li>
+                      <li>
+                        <FontAwesomeIcon
+                          className="is-size-2"
+                          icon={faArrowRight}
+                        />
+                      </li>
+                      <li>Prev</li>
+                    </ul>
                   </div>
                 </section>
               </div>
               <div className="column is-5">
                 <section className="hero next-prev round-corner has-background-white">
                   <div className="hero-body py-4 px-5">
-                    <h1 className="title has-text-primary">December</h1>
+                    <div className="columns">
+                      <div className="column is-8 py-3">
+                        <h1 className="is-size-6 has-text-primary">
+                          March Income
+                        </h1>
+                      </div>
+                      <div className="column is-4 py-3">
+                        <div className="box-percentage is-size-7">
+                          <FontAwesomeIcon icon={faArrowUp} />
+                          7.5%
+                        </div>
+                      </div>
+                    </div>
+                    <div className="columns">
+                      <div className="column pt-0">
+                        <h1 className="title is-size-2 has-text-primary">
+                          0.034768
+                        </h1>
+                      </div>
+                    </div>
+                    <div className="columns">
+                      <div className="column py-1">
+                        <h1 className="title is-size-5 has-text-primary">
+                          ETH
+                        </h1>
+                      </div>
+                    </div>
                   </div>
                 </section>
               </div>
@@ -179,14 +253,47 @@ function App() {
               <div className="column is-7">
                 <section className="hero next-prev round-corner has-background-white">
                   <div className="hero-body py-4 px-5">
-                    <h1 className="title has-text-primary">test</h1>
+                    <h1 className="has-text-primary title-underline">
+                      Detail Cost
+                    </h1>
+                    <table className="table has-text-primary is-size-7">
+                      <tbody>
+                        <tr>
+                          <th>1.</th>
+                          <td>Sata Cable</td>
+                          <td>0.00003</td>
+                          <td>ETH</td>
+                        </tr>
+                        <tr>
+                          <th>1.</th>
+                          <td>Sata Cable</td>
+                          <td>0.00003</td>
+                          <td>ETH</td>
+                        </tr>
+                        <tr>
+                          <th>1.</th>
+                          <td>Sata Cable</td>
+                          <td>0.00003</td>
+                          <td>ETH</td>
+                        </tr>
+                      </tbody>
+                    </table>
                   </div>
                 </section>
               </div>
               <div className="column is-5">
                 <section className="hero next-prev round-corner has-background-white">
                   <div className="hero-body py-4 px-5">
-                    <h1 className="title has-text-primary">test</h1>
+                    <h1 className="has-text-primary title-underline">About</h1>
+                    <p className="has-text-primary is-size-7">
+                      Design by ianfebi01, follow me for more design.
+                    </p>
+                    <ul>
+                      <li className="has-text-primary is-size-7 email">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                        <p className="email-text">ianfebi01@gmail.com</p>
+                      </li>
+                    </ul>
                   </div>
                 </section>
               </div>
